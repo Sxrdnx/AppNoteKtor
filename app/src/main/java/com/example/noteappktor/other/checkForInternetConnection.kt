7a95c6ec6e2118a60935f1c514af1a706bb377ef
Sxrdnx <serdna.alejandro@gmail.com>
2022-02-15 18:57:50 -0600

    handlerInternetConnection_p2_ok

--- app/src/main/java/com/example/noteappktor/other/checkForInternetConnection.kt
+++ app/src/main/java/com/example/noteappktor/other/checkForInternetConnection.kt
@@ -2,10 +2,19 @@ package com.example.noteappktor.other
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Build
 
 fun checkForInternetConnection(context: Context): Boolean {
-    val conectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
         as ConnectivityManager
- return false
+    val activeNetwork = connectivityManager.activeNetwork ?: return false
+    val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+    return when{
+        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+        capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> true
+        else-> false
+    }
 }
